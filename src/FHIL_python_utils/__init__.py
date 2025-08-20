f9cb0e4c0297a28ee99a2fd2e88366ab389bb04f
derrik-gratz <derrik.gratz@gmail.com>
2025-08-20 13:55:29 -0700

    clean imports, versioning

--- src/FHIL_python_utils/__init__.py
+++ src/FHIL_python_utils/__init__.py
@@ -12,38 +12,38 @@ This package provides tools for:
 """
 
 # Import common dependencies at package level for use across all submodules
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from typing import Dict, List, Tuple, Union, Optional, Callable
-from anndata import AnnData
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from typing import Dict, List, Tuple, Union, Optional, Callable
+# from anndata import AnnData
 
-# Import functions from submodules
-from .svm import (
-    featureWeightHeatmap,
-    classesFromProba,
-    annotateClustersByConsensus,
-    confusionBubbleMatrix
-)
+# # Import functions from submodules
+# from .svm import (
+#     featureWeightHeatmap,
+#     classesFromProba,
+#     annotateClustersByConsensus,
+#     confusionBubbleMatrix
+# )
 
-from .plotting import (
-    confusionMatrix,
-    anndataGridplot,
-    rotateAxisLabels
-)
+# from .plotting import (
+#     confusionMatrix,
+#     anndataGridplot,
+#     rotateAxisLabels
+# )
 
-# Define what gets imported with "from FHIL_python_utils import *"
-__all__ = [
-    # SVM functions
-    'featureWeightHeatmap',
-    'classesFromProba', 
-    'annotateClustersByConsensus',
-    'confusionBubbleMatrix',
-    # Plotting functions
-    'confusionMatrix',
-    'anndataGridplot',
-    'rotateAxisLabels',
-    # Common packages (available as package.package_name)
-    'np', 'pd', 'plt', 'sns', 'AnnData'
-]
+# # Define what gets imported with "from FHIL_python_utils import *"
+# __all__ = [
+#     # SVM functions
+#     'featureWeightHeatmap',
+#     'classesFromProba', 
+#     'annotateClustersByConsensus',
+#     'confusionBubbleMatrix',
+#     # Plotting functions
+#     'confusionMatrix',
+#     'anndataGridplot',
+#     'rotateAxisLabels',
+# ]
+
+from ._version import version as __version__
