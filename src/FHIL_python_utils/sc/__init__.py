5def0ebf50d8e9b9c63f85d0a911608d9ff90b32
derrik-gratz <derrik.gratz@gmail.com>
2025-12-08 17:10:06 -0800

    expose multi_object_embedding_scatter.py

--- src/FHIL_python_utils/sc/__init__.py
+++ src/FHIL_python_utils/sc/__init__.py
@@ -5,9 +5,11 @@ Utilities for single-cell analyses
 from .nmad_filter import nmad_filter
 from .ridge_plot import ridge_plot
 from .daniel_processing import daniel_processing
+from .multi_object_embedding_scatter import multi_object_embedding_scatter
 
 __all__ = [
     'nmad_filter',
     'ridge_plot',
-    'daniel_processing'
+    'daniel_processing',
+    'multi_object_embedding_scatter'
 ]
